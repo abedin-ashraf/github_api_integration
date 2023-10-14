e8359adf12b53b79b3962b1e79b2a8b95a3ae2b7
Mohammed Nurul Abedin Ashraf <107454977+abedin-ashraf@users.noreply.github.com>
2023-10-13 21:43:10 -0500

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -12,6 +12,7 @@
 #
 ################################
 
+helper()
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -46,6 +47,12 @@ function list_users_with_read_access {
         echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
         echo "$collaborators"
     fi
+}
+
+function helper{
+expected_cmd_args = 2
+if [ $# -ne $expected_cmd_args]; then
+echo "Please execute the script with required cmd args"
 }
 
 # Main script
